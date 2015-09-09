3e994fc8600016f0df71fa8cd8a1da57a5c7dcf8
Julie Buser <jbuser2013@gmail.com>
2015-09-09 02:24:17 -0500

    business card exercise

---
+++ public/js/modelTest.js
@@ -0,0 +1,15 @@
+(function()  {
+	"use strict";
+	
+	var app = angular.module("modelTest", []);
+
+	angular.module('ui.bootstrap.demo').controller('DropdownCtrl', function ($scope, $log) {
+	  $scope.items = [
+	    'The first choice!',
+	    'And another choice for you.',
+	    'but wait! A third!'
+	  ];
+
+
+
+})();
