88b058b38ac0c9a81bb9d7c87f56c9d42feb2a1d
Julie Buser <jbuser2013@gmail.com>
2015-09-09 02:21:55 -0500

    uploading prior exercise angular js

---
+++ public/js/templateTest.js
@@ -0,0 +1,10 @@
+(function()  {
+	"use strict";
+	/*convention to create variable called app to declare
+	assign to variable and pass in string and empty array*/
+	var app = angular.module("templateTest", []);
+	/* need to bind/tell Angular we want this module bound to our html
+	need a single module that pushes everything else BY adding - ng-app in body tag of html*/
+
+
+})();
